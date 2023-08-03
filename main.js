ba064934d5c91acc06fa362258a61b09f90796e2
carlos0716 <chulezyc@gmail.com>
2023-08-02 18:58:42 -0600

    adds eventListener to the grid function

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 const gridContainer = document.getElementById('grid-container');
 
+
+
 function createGrid(n) {
     
     for (let i = 0; i < n; i++) {
         if (n > 100) return;
         
         for (let j = 0; j < n; j++) {
-            const div = document.createElement('div');
+            let div = document.createElement('div');
             div.classList.add('grid-item');
+            div.classList.add('effect');
+            div.setAttribute('id', 'test')
             gridContainer.appendChild(div);  
             div.style.width = `calc(100%/${n})`;
             div.style.height = `calc(100%/${n})`;
-            
+            div.addEventListener('mouseenter', (e) => {
+                div.style.backgroundColor = 'black';
+            });
         }
     } 
 
     
 }
 
-// function appendChild() {
-//     const div = document.createElement('div');
-//     div.classList.add('grid-item');
-//     gridContainer.appendChild(div);
-// }
-
+let div = document.getElementById('test');
+div.addEventListener('mouseenter', (e) => {
+    div.style.backgroundColor = 'red';
+});
