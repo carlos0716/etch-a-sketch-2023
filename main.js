dfe57e08bf961c1953bb8d0bae5ac84ca8037d1e
carlos0716 <chulezyc@gmail.com>
2023-08-03 12:17:13 -0600

    Event listener added to each button to perfom different actions

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const gridContainer = document.getElementById('grid-container');
-const btn = document.getElementById('btn');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
 
-btn.addEventListener('click', (e)=>{
+btn1.addEventListener('click', (e)=>{
     gridContainer.innerHTML = '';
     let n = prompt('Enter a number between 2 and 100');
-    createGrid(n);
-})
+    createGridRandomColor(n);
+});
 
+btn2.addEventListener('click', (e) => {
+    gridContainer.innerHTML = '';
+    let n = prompt('Enter a number between 2 and 100');
+    createGridBlackAndWhite(n);
+});
 
-function createGrid(n) {
+btn3.addEventListener('click', (e)=> {
+    gridContainer.innerHTML = '';
+});
+
+function createGridRandomColor(n) {
    
     for (let i = 0; i < n; i++) {
         if (n > 100) return;
@@ -21,10 +32,37 @@ function createGrid(n) {
             gridContainer.appendChild(div);  
             div.style.width = `calc(100%/${n})`;
             div.style.height = `calc(100%/${n})`;
+            
+            div.addEventListener('mouseover', (e) => {
+                div.style.backgroundColor = randomColor();
+            });
+        }
+    } 
+}
+
+function createGridBlackAndWhite(n) {
+   
+    for (let i = 0; i < n; i++) {
+        if (n > 100) return;
+        
+        for (let j = 0; j < n; j++) {
+            let div = document.createElement('div');
+            div.classList.add('grid-item');
+            div.classList.add('effect');
+            div.setAttribute('id', 'test')
+            gridContainer.appendChild(div);  
+            div.style.width = `calc(100%/${n})`;
+            div.style.height = `calc(100%/${n})`;
+            
             div.addEventListener('mouseover', (e) => {
                 div.style.backgroundColor = 'black';
             });
         }
     } 
+}
+
+function randomColor()  {
+let rC = '#' +Math.floor(Math.random()*16777215).toString(16);
+return rC;
 }
 
